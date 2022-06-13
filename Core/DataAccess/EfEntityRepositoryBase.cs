21c3bd4bde085823290672134b6037fc19d7ab87
Murat <Muratsahh_61@outlook.com>
2022-06-13 18:09:47 +0300

    EfRepo'ları Olusturuldu

---
+++ Core/DataAccess/EfEntityRepositoryBase.cs
@@ -0,0 +1,71 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public class EfEntityRepositoryBase<T, TContext> : IEntitiyRepository<T>
+        where T : class, IEntitiy, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(T Entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(Entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(T Entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(Entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<T>().SingleOrDefault(filter);
+            }
+        }
+        public List<T> GetAll(Expression<Func<T, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                //if (filter == null)
+                //    return context.Set<Product>().ToList();
+                //else
+                //{
+
+                //}
+                //Nullsa burası                                      // Değilse burası where filter ile filtreyi uyguluyoruz 
+                return filter == null
+                    ? context.Set<T>().ToList()
+                    : context.Set<T>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(T Entity)
+        {
+
+            using (TContext context = new TContext()) //içinde yazılan nesneler bittiğinde bellekten anında atılır
+            {
+                var updateddEntity = context.Entry(Entity);
+                updateddEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Core/DataAccess/IEntitiyRepository.cs
+++ Core/DataAccess/IEntitiyRepository.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.DataAccess
 {
-    public interface IEntitiyRepository
+    public interface IEntitiyRepository<T> where T:class,IEntitiy
     {
-
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T Entity);
+        void Update(T Entity);
+        void Delete(T Entity);
     }
 }
